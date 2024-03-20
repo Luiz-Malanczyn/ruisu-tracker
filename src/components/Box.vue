48916cd5dbdb8d993c91b176fbb734c996ee9fd9
Luiz Eduardo <oliveiraluiz.com@hotmail.com>
2024-03-20 03:06:09 -0300

    Primeiro commit projeto Alura Tracker, utilizando vue.js

---
+++ src/components/Box.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent ({
+    name: 'BoxTarefa'
+})
+
+</script>
+
+<style scoped>
+.box {
+    background: #12208b;
+    color: white;
+}
+</style>
